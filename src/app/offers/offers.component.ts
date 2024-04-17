f49122c54c9fa38d9b60c09e4b9429abbb5af36e
Administrator <alexandruosman1996@gmail.com>
2024-04-17 14:28:51 +0300

    asdasd

--- src/app/offers/offers.component.ts
+++ src/app/offers/offers.component.ts
@@ -4,12 +4,13 @@ import { OfferModel } from '../models/offer.model';
 import { NgFor} from '@angular/common';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-offers',
   standalone: true,
-  imports: [NgFor,MatTableModule,MatButtonModule],
+  imports: [NgFor,MatTableModule,MatButtonModule,MatIconModule],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css'
 })
