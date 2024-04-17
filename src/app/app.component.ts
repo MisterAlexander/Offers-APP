0180650cd464ce8655677357b8550d7be29e8c42
Administrator <alexandruosman1996@gmail.com>
2024-04-17 12:54:08 +0300

    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OffersComponent } from "./offers/offers.component";
 import { HttpClientModule } from '@angular/common/http';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, OffersComponent,HttpClientModule]
+    imports: [RouterOutlet, OffersComponent,HttpClientModule,MatSlideToggleModule]
 })
 export class AppComponent {
   title = 'Offers-app';
